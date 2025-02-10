93c9472c0a64bc0d510255a9c3f36287547e850a
Rakshith Poojary <rakshithp7@gmail.com>
2025-02-09 22:26:55 -0500

    fetch books from DB

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,21 @@
+import { auth } from '@/auth';
 import BookList from '@/components/BookList';
 import BookOverview from '@/components/BookOverview';
 import { Button } from '@/components/ui/button';
-import { sampleBooks } from '@/constants';
 import { db } from '@/database/drizzle';
-import { users } from '@/database/schema';
+import { books, users } from '@/database/schema';
+import { desc } from 'drizzle-orm';
 import Image from 'next/image';
 
 export default async function Home() {
-  // const result = await db.select().from(users);
-  // console.log(JSON.stringify(result, null, 2));
+  const session = await auth();
+
+  const latestBooks = (await db.select().from(books).limit(10).orderBy(desc(books.createdAt))) as Book[];
 
   return (
     <>
-      <BookOverview {...sampleBooks[0]} />
-      <BookList title="Latest Books" books={sampleBooks} containerClassName="mt-28" />
+      <BookOverview {...latestBooks[0]} userId={session?.user?.id as string} />
+      <BookList title="Latest Books" books={latestBooks.slice(1)} containerClassName="mt-28" />
     </>
   );
 }
--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import { cn } from '@/lib/utils';
-import Image from 'next/image';
 import React from 'react';
 import BookCoverSvg from './BookCoverSvg';
+import { IKImage } from 'imagekitio-next';
+import config from '@/lib/config';
 
 type BookCoverVariant = 'extraSmall' | 'small' | 'regular' | 'medium' | 'wide';
 
@@ -30,7 +33,15 @@ const BookCover = ({
     <div className={cn('relative transition-all duration-300', variantStyles[variant], className)}>
       <BookCoverSvg coverColor={coverColor} />
       <div className="absolute z-10" style={{ left: '12%', width: '87.5%', height: '88%' }}>
-        <Image src={coverUrl} alt="Book cover" fill className="rounded-sm object-fill" />
+        <IKImage
+          path={coverUrl}
+          urlEndpoint={config.env.imagekit.urlEndpoint}
+          alt="Book cover"
+          fill
+          className="rounded-sm object-fill"
+          loading="lazy"
+          lqip={{ active: true }}
+        />
       </div>
     </div>
   );
